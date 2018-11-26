19fe70f392932e23142f3aa5d7b7601f0d563a78
KhoaPham <phamdangkhoa0403@gmail.com>
2018-11-26 23:30:25 +0700

    Upload Project to Git

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{   [Authorize]
+    public class HomeController : Controller
+    {
+        public PartialViewResult Index()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult Newtask()
+        {
+            return PartialView();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
